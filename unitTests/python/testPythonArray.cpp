ef5e48c8c2183b78b33ace971cccc2d8fc6c81c1
James Corbett <corbett8@llnl.gov>
2020-07-23 14:13:59 -0700

    Condense python unit tests into fewer files

--- unitTests/python/testPythonArray.cpp
+++ unitTests/python/testPythonArray.cpp
@@ -1,112 +1,206 @@
+#define PY_SSIZE_T_CLEAN
+#define NPY_NO_DEPRECATED_API NPY_1_15_API_VERSION
+#define PY_ARRAY_UNIQUE_SYMBOL LvArray_ARRAY_API
+
 #include "Array.hpp"
 #include "MallocBuffer.hpp"
 #include "python/numpyArrayView.hpp"
-#include "testPythonArray.h"
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <Python.h>
+#include <numpy/arrayobject.h>
 
 // global Array of ints
-static LvArray::Array< long long, 1, RAJA::PERM_I, std::ptrdiff_t, LvArray::MallocBuffer > array1dInt( 30 );
+static LvArray::Array< long long, 1, RAJA::PERM_I, std::ptrdiff_t, LvArray::MallocBuffer > array1d_int( 30 );
 // global 4D Array of doubles
-static LvArray::Array< double, 4, RAJA::PERM_LKIJ, std::ptrdiff_t, LvArray::MallocBuffer > array4dDouble( 5, 6, 7, 8 );
+static LvArray::Array< double, 4, RAJA::PERM_LKIJ, std::ptrdiff_t, LvArray::MallocBuffer > array4d_double( 5, 6, 7, 8 );
 // global 2D Array of chars
-static LvArray::Array< char, 2, RAJA::PERM_JI, std::ptrdiff_t, LvArray::MallocBuffer > array2dChar( 3, 4 );
+static LvArray::Array< char, 2, RAJA::PERM_JI, std::ptrdiff_t, LvArray::MallocBuffer > array2d_char( 3, 4 );
 
 /**
- * Initialize and return a Numpy view of the global 1D-Array of ints.
+ * Fetch the global 1D array and return a numpy view of it.
  */
-PyObject * initArray1dInt( int offset){
-    forValuesInSlice( array1dInt.toSlice(), [&offset]( long long & value )
+static PyObject *
+get_array1d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    LVARRAY_UNUSED_VARIABLE( args );
+    return LvArray::python::create( array1d_int.toView(), true );
+}
+
+/**
+ * Clear and initialize the global 1D Array with values beginning at
+ * an integer offset.
+ * NOTE: this may invalidate previous views of the array!
+ */
+static PyObject *
+set_array1d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int offset;
+    if ( !PyArg_ParseTuple( args, "i", &offset ) )
+        return NULL;
+    forValuesInSlice( array1d_int.toSlice(), [&offset]( long long & value )
     {
         value = offset++;
     } );
-    return getArray1dInt();
+    return get_array1d( nullptr, nullptr );
 }
 
 /**
- * Fetch a Numpy view of the global 1D-Array of ints.
- * Note this view may be invalidated by subsequent calls to `initArray1dInt`
+ * Multiply the global Array by a factor. Return None.
  */
-PyObject * getArray1dInt( void ){
-    return LvArray::python::create( array1dInt.toView(), true );
-}
-
-/**
- * Multiply the global 1D Array of ints
- * @param factor the multiplication factor
- */
-void multiplyArray1d( int factor ){
-    for (long long& i : array1dInt)
+static PyObject *
+multiply_array1d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int factor;
+    if ( !PyArg_ParseTuple( args, "i", &factor ) )
+        return NULL;
+    for (long long& i : array1d_int)
     {
         i = factor * i;
     }
+    Py_RETURN_NONE;
 }
 
 /**
- * Initialize and return a Numpy view of the global 4D-Array of doubles.
+ * Fetch the global SortedArray of floats and return a numpy view of it.
  */
-PyObject * initArray4dDouble( int offset ){
-    forValuesInSlice( array4dDouble.toSlice(), [&offset]( double & value )
+static PyObject *
+get_array4d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    LVARRAY_UNUSED_VARIABLE( args );
+    return LvArray::python::create( array4d_double.toView(), true );
+}
+
+/**
+ * Clear and initialize the global SortedArray of floats with `range(start, stop)`
+ * NOTE: this may invalidate previous views of the array!
+ */
+static PyObject *
+set_array4d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int offset;
+    if ( !PyArg_ParseTuple( args, "i", &offset ) )
+        return NULL;
+    forValuesInSlice( array4d_double.toSlice(), [&offset]( double & value )
     {
         value = offset++;
     } );
-    return getArray4dDouble();
+    return get_array4d( nullptr, nullptr );
 }
 
 /**
- * Fetch a Numpy view of the global 4D-Array of doubles.
- * Note this view may be invalidated by subsequent calls to `initArray4dLongLong`
+ * Multiply the global Array by a factor. Return None.
  */
-PyObject * getArray4dDouble( void ){
-    return LvArray::python::create( array4dDouble.toView(), true );
-}
-
-/**
- * Multiply the global 4D Array of doubles
- * @param factor the multiplication factor
- */
-void multiplyArray4dDouble( int factor ){
-    for (double& i : array4dDouble)
+static PyObject *
+multiply_array4d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int factor;
+    if ( !PyArg_ParseTuple( args, "i", &factor ) )
+        return NULL;
+    for (double& i : array4d_double)
     {
         i = factor * i;
     }
+    Py_RETURN_NONE;
 }
 
 /**
- * Initialize and return a Numpy view of the global 2D-Array of chars.
+ * Fetch the global SortedArray of floats and return a numpy view of it.
  */
-PyObject * initArray2dChar( int offset ){
-    forValuesInSlice( array2dChar.toSlice(), [&offset]( char & value )
+static PyObject *
+get_array2d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    LVARRAY_UNUSED_VARIABLE( args );
+    return LvArray::python::create( array2d_char.toView(), true );
+}
+
+/**
+ * Clear and initialize the global SortedArray of floats with `range(start, stop)`
+ * NOTE: this may invalidate previous views of the array!
+ */
+static PyObject *
+set_array2d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int offset;
+    if ( !PyArg_ParseTuple( args, "i", &offset ) )
+        return NULL;
+    forValuesInSlice( array2d_char.toSlice(), [&offset]( char & value )
     {
         value = offset++;
     } );
-    return getArray2dChar();
+    return get_array2d( nullptr, nullptr );
 }
 
 /**
- * Fetch a Numpy view of the global 2D-Array of chars.
- * Note this view may be invalidated by subsequent calls to `initArray4dLongLong`
+ * Multiply the global Array by a factor. Return None.
  */
-PyObject * getArray2dChar( void ){
-    return LvArray::python::create( array2dChar.toView(), true );
-}
-
-/**
- * Multiply the global 2D-Array of chars
- * @param factor the multiplication factor
- */
-void multiplyArray2dChar( int factor ){
-    for (char & i : array2dChar)
+static PyObject *
+multiply_array2d( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int factor;
+    if ( !PyArg_ParseTuple( args, "i", &factor ) )
+        return NULL;
+    for (char & i : array2d_char)
     {
         i = factor * i;
     }
+    Py_RETURN_NONE;
 }
 
-#ifdef __cplusplus
+/**
+ * Array of functions and docstrings to export to Python
+ */
+static PyMethodDef LvArrayFuncs[] = {
+    {"set_array1d",  set_array1d, METH_VARARGS,
+     "Return the numpy representation of a the global 1D Array after initializing it `range(start, stop)`."},
+    {"get_array1d",  get_array1d, METH_NOARGS,
+     "Get the numpy representation of the global 1D Array."},
+    {"multiply_array1d",  multiply_array1d, METH_VARARGS,
+     "Multiply the contents of the global 1D Array."},
+    {"set_array4d",  set_array4d, METH_VARARGS,
+     "Return the numpy representation of a the global 4D Array after initializing it `range(start, stop)`."},
+    {"get_array4d",  get_array4d, METH_NOARGS,
+     "Get the numpy representation of the global 4D Array."},
+    {"multiply_array4d",  multiply_array4d, METH_VARARGS,
+     "Multiply the contents of the global 4D Array."},
+    {"set_array2d",  set_array2d, METH_VARARGS,
+     "Return the numpy representation of a the global 2D Array after initializing it `range(start, stop)`."},
+    {"get_array2d",  get_array2d, METH_NOARGS,
+     "Get the numpy representation of the global 2D Array."},
+    {"multiply_array2d",  multiply_array2d, METH_VARARGS,
+     "Multiply the contents of the global 2D Array."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+
+/**
+ * Initialize the module object for Python with the exported functions
+ */
+static struct PyModuleDef _testLvArrayPythonInterfacemodule = {
+    PyModuleDef_HEAD_INIT,
+    "testPythonArray",   /* name of module */
+    "Module for testing numpy views of LvArray::Array objects", /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    LvArrayFuncs,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+
+PyMODINIT_FUNC
+PyInit_testPythonArray(void)
+{
+    import_array();
+    return PyModule_Create(&_testLvArrayPythonInterfacemodule);
 }
-#endif
-
-
--- unitTests/python/testPythonSortedArray.cpp
+++ unitTests/python/testPythonSortedArray.cpp
@@ -1,47 +1,121 @@
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2019, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-746361
+ *
+ * All rights reserved. See COPYRIGHT for details.
+ *
+ * This file is part of the GEOSX Simulation Framework.
+ *
+ * GEOSX is a free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License (as published by the
+ * Free Software Foundation) version 2.1 dated February 1999.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#define PY_SSIZE_T_CLEAN
+#define NPY_NO_DEPRECATED_API NPY_1_15_API_VERSION
+#define PY_ARRAY_UNIQUE_SYMBOL LvArray_ARRAY_API
+
 #include "SortedArray.hpp"
 #include "MallocBuffer.hpp"
 #include "python/numpySortedArrayView.hpp"
-#include "testPythonSortedArray.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <Python.h>
+#include <numpy/arrayobject.h>
 
 // global SortedArray of ints
-LvArray::SortedArray< int, std::ptrdiff_t, LvArray::MallocBuffer > sortedArrayInt;
+static LvArray::SortedArray< int, std::ptrdiff_t, LvArray::MallocBuffer > sorted_arr_int;
 
 /**
- * Initialize and return a Numpy view of the global SortedArray of ints.
+ * Fetch the global SortedArray and return a numpy view of it.
  */
-PyObject * initSortedArrayInt( int start, int stop ){
-    sortedArrayInt.clear();
+static PyObject *
+get_sorted_array_int( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    LVARRAY_UNUSED_VARIABLE( args );
+    return LvArray::python::create( sorted_arr_int.toView(), true );
+}
+
+/**
+ * Clear and initialize the global SortedArray with `range(start, stop)`
+ * NOTE: this may invalidate previous views of the array!
+ */
+static PyObject *
+set_sorted_array_int( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int start;
+    int stop;
+    if ( !PyArg_ParseTuple( args, "ii", &start, &stop ) )
+        return NULL;
+    if ( stop <= start ){
+        Py_RETURN_NONE;
+    }
+    sorted_arr_int.clear();
     for ( int i = start; i < stop; ++i )
     {
-        sortedArrayInt.insert( i );
+        sorted_arr_int.insert( i );
     }
-    return getSortedArrayInt();
+    return get_sorted_array_int( nullptr, nullptr );
 }
 
 /**
- * Fetch a Numpy view of the global SortedArray of ints.
- * Note this view may be invalidated by subsequent calls to `initSortedArrayInt`
+ * Multiply the global SortedArray by a factor. Return None.
  */
-PyObject * getSortedArrayInt( void ){
-    return LvArray::python::create( sortedArrayInt.toView(), true );
-}
-
-/**
- * Multiply the global SortedArray of ints by a factor
- */
-void multiplySortedArrayInt( int factor ){
+static PyObject *
+multiply_sorted_array_int( PyObject *self, PyObject *args )
+{
+    LVARRAY_UNUSED_VARIABLE( self );
+    int factor;
+    if ( !PyArg_ParseTuple( args, "i", &factor ) )
+        return NULL;
     LvArray::SortedArray< int, std::ptrdiff_t, LvArray::MallocBuffer > newSortedArray;
-    for (int i : sortedArrayInt)
+    for (int i : sorted_arr_int)
     {
         newSortedArray.insert( factor * i );
     }
-    sortedArrayInt = newSortedArray;
+    sorted_arr_int = newSortedArray;
+    Py_RETURN_NONE;
 }
 
-#ifdef __cplusplus
+/**
+ * Array of functions and docstrings to export to Python
+ */
+static PyMethodDef SortedArrayFuncs[] = {
+    {"set_sorted_array_int",  set_sorted_array_int, METH_VARARGS,
+     "Return the numpy representation of a SortedArray initialized like `range(start, stop)`."},
+    {"get_sorted_array_int",  get_sorted_array_int, METH_NOARGS,
+     "Get the numpy representation of a SortedArray."},
+    {"multiply_sorted_array_int",  multiply_sorted_array_int, METH_VARARGS,
+     "Multiply the contents of a SortedArray."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+/**
+ * Initialize the module object for Python with the exported functions
+ */
+static struct PyModuleDef testPythonSortedArraymodule = {
+    PyModuleDef_HEAD_INIT,
+    "testPythonArray",   /* name of module */
+    /* module documentation, may be NULL */
+    "Module for testing numpy views of LvArray::SortedArray objects",
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    SortedArrayFuncs,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+PyMODINIT_FUNC
+PyInit_testPythonSortedArray(void)
+{
+    import_array();
+    return PyModule_Create(&testPythonSortedArraymodule);
 }
-#endif
